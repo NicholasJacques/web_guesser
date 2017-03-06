319412442902ca2a1af43bd0023272585170b3cf
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-06 15:23:49 -0700

    works, need to add another iteration

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,37 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+class WebGuesser
 
-get '/' do
- erb :index, :locals => {:number => number}
+  attr_reader :secret_number
+
+  def initialize
+    @secret_number = rand(100)
+  end
+
+  def check_guess(guess = nil)
+    if guess.nil?
+      "Make a guess"
+    elsif guess > secret_number + 10
+      "Way too high!"
+    elsif guess > secret_number
+      "Too High!"
+    elsif guess < secret_number - 10
+      "Way too low!"
+    elsif guess < secret_number
+      "Too low!"
+    else
+      "Correct! The SECRET NUMBER is #{secret_number}"
+    end
+  end
+
 end
+
+web_guesser = WebGuesser.new
+get '/' do
+  guess = params["guess"].to_i
+  erb :index, :locals => {:message => web_guesser.check_guess(guess)}
+end
+
+
+
