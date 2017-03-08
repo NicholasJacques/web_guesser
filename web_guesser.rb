0176581fed50fd799c807e19f39030f18d16c3b7
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-03-07 17:56:36 -0700

    completed with guess limit extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,35 +3,54 @@ require 'sinatra/reloader'
 
 class WebGuesser
 
-  attr_reader :secret_number
+  attr_reader :number,
+              :guess_count
 
   def initialize
-    @secret_number = rand(100)
+    @number = rand(1..100)
+    @guess_count = 5
   end
 
-  def check_guess(guess = nil)
-    if guess.nil?
-      "Make a guess"
-    elsif guess > secret_number + 10
-      "Way too high!"
-    elsif guess > secret_number
-      "Too High!"
-    elsif guess < secret_number - 10
-      "Way too low!"
-    elsif guess < secret_number
-      "Too low!"
+  def count_guess
+    @guess_count -=1
+  end
+
+  def reset
+    @guess_count = 5
+    @number = rand(1..100)
+  end
+
+  def check_count
+    if guess_count == 0
+      reset
+      " You Lose! New Number!"
     else
-      "Correct! The SECRET NUMBER is #{secret_number}"
+      ""
     end
   end
 
+  def check_guess(guess)
+    count_guess unless guess == 0
+    if guess > (number + 10)
+      message = "Way too high!"
+    elsif guess > number
+      message = "Too high!"
+    elsif guess < (number - 10)
+      message = "Way too low!"
+    elsif guess < number
+      message = "Too low!"
+    else
+      message = "Correct! The secret number was #{number}!"
+      reset
+    end
+    message + check_count
+  end
 end
 
 web_guesser = WebGuesser.new
+
 get '/' do
   guess = params["guess"].to_i
-  erb :index, :locals => {:message => web_guesser.check_guess(guess)}
+  message = web_guesser.check_guess(guess)
+  erb :index, :locals => { :number => web_guesser.number, :message => message, :guess_count => web_guesser.guess_count }
 end
-
-
-
